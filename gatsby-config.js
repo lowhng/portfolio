7cae287a2bf18f81279b40e31a2ddb6edfd1b6f3
wlo <weihong.edu@outlook.com>
2022-08-14 17:56:44 +1200

    Changed sitename

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ require("dotenv").config({
 })
 module.exports = {
   siteMetadata: {
-    title: "Barcadia",
+    title: "Wei Hong",
     description: "A super-fast site using GatsbyJS",
     author: "Morgan Baker",
     twitterUsername: "barcadia",
